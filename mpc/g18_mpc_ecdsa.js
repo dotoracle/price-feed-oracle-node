cc8bb0f850b4ca9b18e95ec77a0e1a33b20bd9f8
dotoracle-network <dotoracle.network@gmail.com>
2021-07-23 15:28:57 +0700

    add end point SM

--- mpc/g18_mpc_ecdsa.js
+++ mpc/g18_mpc_ecdsa.js
@@ -1,16 +1,23 @@
-const data = "7ca55ad2ad216bd41822ae9f0a1ccd35bd3a5f5517316567cc7f2a05c7d09b8d"
+const argv = process.env.argv.slice(1)
+const data = argv[0]
 const fileName = "signature" + data + ".json"
 const { exec } = require("child_process");
-const smEndPoint = "http://45.32.28.180:8001"
+require('dotenv').config()
+const smEndPoint = process.env.SM_ENDPOINT
 const keyStoreFile = "keys.store"
-exec(`./gg18_sign_client ${smEndPoint} ${keyStoreFile} "${data}" ${fileName}`, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`Done: Signature saved in file ${fileName}`);
-});
+
+function startMPCSign() {
+    exec(`./gg18_sign_client ${smEndPoint} ${keyStoreFile} "${data}" ${fileName}`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Done: Signature saved in file ${fileName}`);
+    });
+}
+
+startMPCSign()
