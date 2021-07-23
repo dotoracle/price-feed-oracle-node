65af765bac1e6e3fb82adfd1e635581bdf7b431c
dotoracle <dotoracle.network@gmail.com>
2021-07-23 08:21:15 +0000

    success sign data MPC

--- mpc/g18_mpc_ecdsa.js
+++ mpc/g18_mpc_ecdsa.js
@@ -1,9 +1,9 @@
 const data = "7ca55ad2ad216bd41822ae9f0a1ccd35bd3a5f5517316567cc7f2a05c7d09b8d"
-const fileName = "signature" + data
+const fileName = "signature" + data + ".json"
 const { exec } = require("child_process");
 const smEndPoint = "http://45.32.28.180:8001"
 const keyStoreFile = "keys.store"
-exec(`gg18_sign_client ${smEndPoint} ${keyStoreFile} "${data}" ${fileName}`, (error, stdout, stderr) => {
+exec(`./gg18_sign_client ${smEndPoint} ${keyStoreFile} "${data}" ${fileName}`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
