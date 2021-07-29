3e3018f0973a053e431431a2e3128224c167bb02
dotoracle-network <dotoracle.network@gmail.com>
2021-07-29 19:10:38 +0700

    add timeout for signing process

--- mpc/g18_mpc_ecdsa.js
+++ mpc/g18_mpc_ecdsa.js
@@ -1,31 +1,24 @@
-const { exec } = require("child_process");
+const spawn = require('child_process').spawn;
+let child = spawn('node', ['child.js'], { timeout: 20000 });
+
 require('dotenv').config()
 const logger = require('../helpers/logger');
 
 function startMPCSign(smEndPoint, keyStoreFile, data, cb) {
     const fileName = "signature" + data + ".json"
     logger.info("Starting MPC")
-    let signProcess = exec(`./mpc/gg18_sign_client ${smEndPoint} mpc/${keyStoreFile} "${data}" mpc/${fileName}`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+    let signProcess = spawn('./mpc/gg18_sign_client', [`${smEndPoint} mpc/${keyStoreFile}`, `"${data}"`, `mpc/${fileName}`], { timeout: 120000 })
+    signProcess.on('close', (code, signal) => {
+        logger.info(
+            `Signing process terminated due to receipt of signal ${signal}`);
+        if (!signal) {
+            console.log(`Done: Signature saved in file ${fileName}`);
+            let sigJson = require(`./${fileName}`)
+            cb({ r: sigJson[1], s: sigJson[3], v: sigJson[5] })
+        } else {
+            logger.warn('Signing process close due to timeout')
         }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`Done: Signature saved in file ${fileName}`);
-        let sigJson = require(`./${fileName}`)
-        cb({r: sigJson[1], s: sigJson[3], v: sigJson[5]})
     });
-    setTimeout(() => {
-        try {
-            logger.info('Killing process')
-            signProcess.kill("SIGINT")
-        } catch (e) {
-
-        }
-    }, 120000)
 }
 
 module.exports = {
