20857a8e32f5d4ac61b622f0ac21c8fb889da74d
dotoracle-network <dotoracle.network@gmail.com>
2021-07-29 19:16:54 +0700

    add timeout for signing process

--- mpc/g18_mpc_ecdsa.js
+++ mpc/g18_mpc_ecdsa.js
@@ -10,8 +10,8 @@ function startMPCSign(smEndPoint, keyStoreFile, data, cb) {
     let signProcess = spawn('./mpc/gg18_sign_client', [`${smEndPoint} mpc/${keyStoreFile}`, `"${data}"`, `mpc/${fileName}`], { timeout: 120000 })
     signProcess.on('close', (code, signal) => {
         logger.info(
-            `Signing process terminated due to receipt of signal ${signal}`);
-        if (!signal) {
+            `Signing process terminated due to receipt of signal ${signal} ${code}`);
+        if (!signal && code == 0) {
             console.log(`Done: Signature saved in file ${fileName}`);
             let sigJson = require(`./${fileName}`)
             cb({ r: sigJson[1], s: sigJson[3], v: sigJson[5] })
