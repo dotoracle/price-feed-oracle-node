3f1a465aa773b4d39f690ca57570704559d85810
dotoracle-network <dotoracle.network@gmail.com>
2021-07-29 19:36:05 +0700

    threshold 66%

--- mpc/g18_mpc_ecdsa.js
+++ mpc/g18_mpc_ecdsa.js
@@ -6,11 +6,11 @@ const logger = require('../helpers/logger');
 function startMPCSign(smEndPoint, keyStoreFile, data, cb) {
     const fileName = "signature" + data + ".json"
     logger.info("Starting MPC")
-    let signProcess = spawn('./mpc/gg18_sign_client', [`${smEndPoint} mpc/${keyStoreFile}`, `"${data}"`, `mpc/${fileName}`], { timeout: 120000 })
+    let signProcess = spawn('./mpc/gg18_sign_client', [`${smEndPoint} mpc/${keyStoreFile}`, `"${data}"`, `mpc/${fileName}`], { timeout: 120000, killSignal: "SIGINT" })
     signProcess.on('close', (code, signal) => {
         logger.info(
             `Signing process terminated due to receipt of signal ${signal} ${code}`);
-        if (!signal && code == 0) {
+        if (!signal) {
             console.log(`Done: Signature saved in file ${fileName}`);
             let sigJson = require(`./${fileName}`)
             cb({ r: sigJson[1], s: sigJson[3], v: sigJson[5] })
